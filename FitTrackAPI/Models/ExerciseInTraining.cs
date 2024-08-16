8fb091d8e32d1edd4a10b7b2a6d2a73f572c04ad
Gravelino <148808322+Gravelino@users.noreply.github.com>
2024-08-16 19:19:59 +0300

    object model changes
    deleted not needed fields in exercises_in_training object model

--- FitTrackAPI/Models/ExerciseInTraining.cs
+++ FitTrackAPI/Models/ExerciseInTraining.cs
@@ -2,10 +2,7 @@ public class ExerciseInTraining
 {
     public int Id { get; set; }          
     public int TrainingId { get; set; }  
-    public int ExerciseId { get; set; } 
-    public int UserId { get; set; }      
-
+    public int ExerciseId { get; set; }  
     public Exercise Exercise { get; set; }
     public IndividualTraining IndividualTraining { get; set; }    
-    public User User { get; set; }
 }
