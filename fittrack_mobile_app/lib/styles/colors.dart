ddec33b898efd1dbe2ebecd454121e789b6f4eb6
KaktusSd06 <120324422+KaktusSd06@users.noreply.github.com>
2024-09-14 14:02:14 +0300

    Create project. Add style folder with colors and fonts

---
+++ fittrack_mobile_app/lib/styles/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color(0xFF000000);
+const Color jet = Color(0xFF201F1C);
+const Color gray = Color(0xFF40403B);
+const Color fulvous = Color(0xFFE48100);
+const Color white = Color(0xFFFFFFFF);
+const Color isabelline = Color(0xFFF4F0E9);
+const Color silver = Color(0xFFC6C1B9);
---
+++ fittrack_mobile_app/lib/styles/fonts.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+TextStyle h1 = const TextStyle(
+  fontFamily: 'NotoSans',
+  fontSize: 22,
+  fontWeight: FontWeight. w600,
+);
+
+TextStyle h2 = const TextStyle(
+  fontFamily: 'NotoSans',
+  fontSize: 18,
+  fontWeight: FontWeight. w400,
+);
+
+TextStyle h3 = const TextStyle(
+  fontFamily: 'NotoSans',
+  fontSize: 14,
+  fontWeight: FontWeight. w400,
+);
+
+TextStyle h4 = const TextStyle(
+  fontFamily: 'NotoSans',
+  fontSize: 12,
+  fontWeight: FontWeight. w300,
+);
+
+TextStyle h5 = const TextStyle(
+  fontFamily: 'NotoSans',
+  fontSize: 10,
+  fontWeight: FontWeight. w400,
+);
