71ca66109a5158ada5b886a65eeca2e8964785ae
Gravelino <148808322+Gravelino@users.noreply.github.com>
2024-10-28 17:35:57 +0200

    UserController Update

---
+++ FitTrack.API/FitTrack.API/Models/Update.cs
@@ -0,0 +1,10 @@
+﻿namespace FitTrack.API.Controllers;
+
+public class Update
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; } 
+    public string? MiddleName { get; set; } 
+    public double? Weight { get; set; }
+    public int? Height { get; set; }
+}
