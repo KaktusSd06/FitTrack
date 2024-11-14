04c5ddfada34ddfae977f09b70cda5a6ba280e55
Gravelino <148808322+Gravelino@users.noreply.github.com>
2024-11-14 23:09:22 +0200

    FT-18 add account controller

--- FitTrack.API/FitTrack.API/Controllers/AccountController.cs
+++ FitTrack.API/FitTrack.API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Security.Claims;
 using System.Text;
 using FitTrack.API.Models;
+using FitTrack.API.Services;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Google.Apis.Auth;
@@ -15,13 +16,13 @@ namespace FitTrack.API.Controllers;
 public class AccountController : Controller
 {
     private readonly UserManager<Person> _userManager;
-    private readonly SignInManager<Person> _signInManager;
+    private readonly TokenService _tokenService;
     private readonly IConfiguration _configuration;
 
-    public AccountController(UserManager<Person> userManager, SignInManager<Person> signInManager, IConfiguration configuration)
+    public AccountController(UserManager<Person> userManager, TokenService tokenService, IConfiguration configuration)
     {
         _userManager = userManager;
-        _signInManager = signInManager;
+        _tokenService = tokenService;
         _configuration = configuration;
     }
 
@@ -76,7 +77,7 @@ public class AccountController : Controller
         return BadRequest(creationResult.Errors);
     }*/
 
-    [HttpPost("login-google")]
+    /*[HttpPost("login-google")]
     public async Task<IActionResult> GoogleLogin([FromBody] string googleToken)
     {
         var payload = await ValidateGoogleToken(googleToken);
@@ -135,6 +136,70 @@ public class AccountController : Controller
         }
         
         return await _userManager.FindByIdAsync(newUser.Id);
+    }*/
+    
+    [HttpPut("update-email/{id}")]
+    public async Task<IActionResult> UpdateEmail(string id, [FromBody] string newEmail)
+    {
+        var person = await _userManager.FindByIdAsync(id);
+        if (person == null)
+        {
+            return NotFound();
+        }
+        
+        var token = await _userManager.GenerateChangeEmailTokenAsync(person, newEmail);
+        var result = await _userManager.ChangeEmailAsync(person, newEmail, token);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+        
+        await _userManager.SetUserNameAsync(person, newEmail);
+        
+        return NoContent();
+    }
+
+    [HttpPut("update-phone/{id}")]
+    public async Task<IActionResult> UpdatePhone(string id, [FromBody] string newPhone)
+    {
+        var person = await _userManager.FindByIdAsync(id);
+        if (person == null)
+        {
+            return NotFound();
+        }
+        
+        var token = await _userManager.GenerateChangePhoneNumberTokenAsync(person, newPhone);
+        var result = await _userManager.ChangePhoneNumberAsync(person, newPhone, token);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+        
+        return NoContent();
+    }
+
+    [HttpPut("update-password/{id}")]
+    public async Task<IActionResult> UpdatePassword(string id, [FromBody] string newPassword)
+    {
+        var person = await _userManager.FindByIdAsync(id);
+        if (person == null)
+        {
+            return NotFound();
+        }
+        
+        var removePasswordResult = await _userManager.RemovePasswordAsync(person);
+        if (!removePasswordResult.Succeeded)
+        {
+            return BadRequest(removePasswordResult.Errors);
+        }
+        
+        var setPasswordResult = await _userManager.AddPasswordAsync(person, newPassword);
+        if (!setPasswordResult.Succeeded)
+        {
+            return BadRequest(setPasswordResult.Errors);
+        }
+        
+        return NoContent();
     }
     
     [HttpPost("login")]
@@ -151,30 +216,13 @@ public class AccountController : Controller
             return Unauthorized();
         }
         
-        var token = GenerateJwtToken(user);
+        var token = GenerateJwtTokens(user).Result;
         
-        return Ok(new { Token = token });
-        
-        /*var userRoles = await _userManager.GetRolesAsync(user);
-        
-        var authClaims = new List<Claim>
+        return Ok(new
         {
-            new (JwtRegisteredClaimNames.Sub, user.Email!),
-            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-
-        };
-        
-        authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
-
-        var token = new JwtSecurityToken(
-            issuer: _configuration["Jwt:Issuer"],
-            expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpiryMinutes"]!)),
-            claims: authClaims,
-            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)),
-                SecurityAlgorithms.HmacSha256)
-        );
-        
-        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });*/
+            AccessToken = token.First(),
+            RefreshToken = token.Last()
+        });
     }
 
     [HttpPost("assign-role")]
@@ -217,28 +265,85 @@ public class AccountController : Controller
         return Ok();
     }
     
-    [HttpGet("generate-token")]
-    public async Task<IActionResult> GenerateJwtToken(Person user)
+    [HttpGet("get-all-roles/{userId}")]
+    public async Task<IActionResult> GetAllRoles(string userId)
     {
-        var userRoles = await _userManager.GetRolesAsync(user);
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
         
+        var result = await _userManager.GetRolesAsync(user);
+
+        return Ok(result);
+    }
+
+    [HttpGet("generate-tokens")]
+    public async Task<IEnumerable<string>> GenerateJwtTokens(Person user)
+    {
+        var accessToken = GenerateJwtToken(user, _configuration["Jwt:AccessExpiryMinutes"]!);
+        var refreshToken = GenerateJwtToken(user, _configuration["Jwt:RefreshExpiryDays"]!);
+        
+        return [
+            accessToken,
+            refreshToken,
+        ];
+    }
+    
+    [HttpGet("generate-token")]
+    public string GenerateJwtToken(Person user, string expiry)
+    {
         var authClaims = new List<Claim>
         {
             new (JwtRegisteredClaimNames.Sub, user.Email!),
             new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-
+            new (ClaimTypes.NameIdentifier, user.Id),
         };
-        
+
+        var userRoles = _userManager.GetRolesAsync(user).Result;
         authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
 
         var token = new JwtSecurityToken(
             issuer: _configuration["Jwt:Issuer"],
-            expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpiryMinutes"]!)),
+            audience: _configuration["Jwt:Audience"],
+            expires: DateTime.UtcNow.AddMinutes(double.Parse(expiry)),
             claims: authClaims,
-            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)),
-                SecurityAlgorithms.HmacSha256)
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
         );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    [HttpPost("refresh-tokens")]
+    public async Task<IActionResult> RefreshTokens(string userId, string refreshToken)
+    {
+        if (string.IsNullOrEmpty(refreshToken))
+        {
+            return BadRequest("Invalid refresh token");
+        }
         
-        return Ok(new{Token = new JwtSecurityTokenHandler().WriteToken(token)});
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return BadRequest("Invalid userId");
+        }
+        
+        var isValid = _tokenService.ValidateRefreshToken(refreshToken);
+        if (!isValid)
+        {
+            return Unauthorized();
+        }
+        
+        var newAccessToken = GenerateJwtToken(user, _configuration["Jwt:AccessExpiryMinutes"]!);
+
+        return Ok(new
+        {
+            AccessToken = newAccessToken,
+            RefreshToken = refreshToken,
+        });
     }
 }
